cbcdedcdfc9db77c1b4a1ba45dfc595575c5116e
Joan Roig <12451419+joanroig@users.noreply.github.com>
2020-01-21 20:58:04 +0100

    - All documents added. - Document process changed to enable the order by chapters or by topics. - Lateral menu must have expandable rows.

--- documents.js
+++ documents.js
@@ -1,4 +1,4 @@
-// Update documents.json
+// Update document jsons
 const {
   readdirSync
 } = require('fs');
@@ -7,26 +7,55 @@ var fs = require('fs')
 
 const FOLDER = './src/assets/documents';
 var docs = readdirSync(FOLDER);
-var documents = [];
+var topics = {};
+var chapters = {};
 
 docs.forEach(process);
 
-// console.log(documents);
+write('chapters.json', chapters);
+write('topics.json', topics);
 
-fs.writeFileSync('src/documents.json', JSON.stringify(documents), {
-  encoding: 'utf8',
-  flag: 'w'
-})
-
-console.log('File documents.json updated!')
+console.log('File chapters.json and topics.json updated!')
 
 function process(item, index) {
   if (!item.startsWith(".")) {
-    var doc = {};
-    doc.file = 'assets/documents/' + item;
-    doc.title = item.split("-")[1].slice(0, -5).replace('_', ' ');
-    doc.chapter = Number(item.split("-")[0].split(".")[0]);
-    doc.lesson = Number(item.split("-")[0].split(".")[1]);
-    documents.push(doc);
+
+    // Prepare variables
+    let file = 'assets/documents/' + item;
+    let title = item.split("-")[1].replace(/_/g, ' ').replace('.html', '');
+    let chapter = Number(item.split("-")[0].split(".")[0]);
+    let order = Number(item.split("-")[0].split(".")[1]);
+    var lesson = item.split("-")[2];
+    lesson = lesson == null ? 0 : parseInt(lesson[0]);
+    let fullTitle = lesson == 0 ? title : title + " " + lesson;
+
+    // Build a lesson for a chapter and a lesson for a topic
+    var chapterLesson = {},
+      topicLesson = {};
+    chapterLesson.file = topicLesson.file = file;
+    chapterLesson.title = topicLesson.title = fullTitle;
+    chapterLesson.lesson = order;
+    if (lesson != 0) {
+      topicLesson.lesson = lesson;
+    }
+
+    // Insert the lessons in chapters and topics
+    insert(chapter, chapterLesson, chapters);
+    insert(title, topicLesson, topics);
   }
+}
+
+function insert(id, item, list) {
+  if (list[id]) {
+    list[id].push(item)
+  } else {
+    list[id] = [item]
+  }
+}
+
+function write(file, list) {
+  fs.writeFileSync('src/assets/' + file, JSON.stringify(list), {
+    encoding: 'utf8',
+    flag: 'w'
+  })
 }
