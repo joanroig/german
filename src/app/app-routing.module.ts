efcad6b8a9dbdf6359683a3f0225890fbca88aed
Joan Roig <12451419+joanroig@users.noreply.github.com>
2020-01-22 07:25:45 +0100

    Some enhancements. The topics organization is not working for now. Basic swipe added for mobile.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'chapter/:cid/:id', component: AppComponent },
-  { path: 'topic/:tid/:id', component: AppComponent }
+  { path: 'topic/:tid/:id', component: AppComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+
 ];
 
 @NgModule({
